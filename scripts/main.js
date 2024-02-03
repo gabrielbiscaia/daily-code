3039ece6ef904ff08b1c44c80968b6affb243f5f
gabrielbiscaia <ra118928@uem.br>
2024-02-03 17:52:03 -0300

    atualização do preload

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,30 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron/main");
 const path = require("node:path");
 
-let mainWindow;
-
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
-    resizable: false
+    resizable: false,
+    webPreferences: {
+      preload: path.join('', 'preload.js'),
+    }
   });
 
   mainWindow.loadFile("src/menu.html");
 };
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+});
+
 //Remove a BUG of Vsync
 app.disableHardwareAcceleration();
-
-ipcMain.on("load-game", () => {
-  // Aqui você carrega o conteúdo do game.html
-  mainWindow.loadFile("src/game.html");
-});
