eb66f3d9f70c1277fab39ca4d421f264579129c3
gabrielbiscaia <ra118928@uem.br>
2024-02-21 20:39:15 -0300

    divisão do game.js para maior organização

---
+++ scripts/audioData.js
@@ -0,0 +1,19 @@
+const audioFiles = {
+    // Audios do piter
+    piter_comemoracao: new Audio('../audio/character/piter-comemoracao.wav'),
+    piter_entendeu: new Audio('../audio/character/piter-entendeu.wav'),
+    piter_erro: new Audio('../audio/character/piter-erro.wav'),
+    piter_feliz: new Audio('../audio/character/piter-feliz.wav'),
+    piter_irritado: new Audio('../audio/character/piter-irritado.wav'),
+    piter_surpreso: new Audio('../audio/character/piter-surpreso.wav'),
+    // Audios transição
+    alarme: new Audio('../audio/environment/alarme.wav'),
+    cafe: new Audio('../audio/environment/cafe.wav'),
+    carro: new Audio('../audio/environment/carro.wav'),
+    porta: new Audio('../audio/environment/porta.wav'),
+    restaurante: new Audio('../audio/environment/restaurante.wav'),
+    // Audio background
+    background: new Audio('../audio/background/background-music.wav')
+};
+
+export { audioFiles };
