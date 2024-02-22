d9a98db97caeba50decc1b6ff5869519b10ff09d
gabrielbiscaia <ra118928@uem.br>
2024-02-22 16:13:01 -0300

    varias melhorias e features novas, jogo funcionando

--- scripts/game.js
+++ scripts/game.js
@@ -13,22 +13,25 @@ const img_character2 = document.getElementById("img-character2");
 const speaker = document.getElementById("speaker");
 const box_question = document.getElementById("box-question");
 const box_question_text = document.getElementById("box-question-text");
-var blackScreen = document.getElementById("black-screen");
-var input_answer = document.getElementById("input-answer")
+const blackScreen = document.getElementById("black-screen");
+
+// Instancinado variaveis relacionados as pergunatas
+const totalQuestions = questionData.length;
+var questionNow;
+var totalCorrectAnswers = 0;
 
 
 // Começo do jogo
 callBlackScreen(0, 2500, 2000, false)
-audioFiles.alarme.volume = 0.3;
-playAudio(audioFiles.alarme);
-audioFiles.background.volume = 0.06;
+playAudio(audioFiles.alarme, 0.3);
 audioFiles.background.loop = true;
-playAudio(audioFiles.background);
+playAudio(audioFiles.background, 0.06);
 setTimeout(function(){
     pauseAudio(audioFiles.alarme)
 }, 2200)
 
-function playAudio(audio) {
+function playAudio(audio, volume) {
+    audio.volume = volume;
     audio.play();
 }
 
@@ -44,6 +47,10 @@ function disableArrow(){
     btn_arrow.style.display = 'none';
 }
 
+function hideBoxQuestion(){
+    box_question.style.display = "none"
+}
+
 function nextStep() {
     if (gameData.length > 0) {
 
@@ -55,7 +62,7 @@ function nextStep() {
             if(nextData.thereIsQuestion){
                 showQuestion();
             }else{
-                box_question.style.display = "none"
+                hideBoxQuestion();
             }
         }
 
@@ -65,42 +72,66 @@ function nextStep() {
         if (nextData.img_character1 === "") {
             img_character1.style.display = "none";
         } else {
-            img_character1.style.display = "block";
             img_character1.src = nextData.img_character1;
+            img_character1.style.display = "block";
         }
 
         if (nextData.img_character2 === "") {
             img_character2.style.display = "none";
         } else {
-            img_character2.style.display = "block";
             img_character2.src = nextData.img_character2;
+            img_character2.style.display = "block";
         }
 
         if (nextData.audio != "") {
-            nextData.audio.volume = 0.3;
             setTimeout(function(){
-                playAudio(nextData.audio);
+                playAudio(nextData.audio, 0.3);
             },200)
         }
 
-        typewriter
-            .callFunction(()=>{
-                disableArrow()
-            })
-            .deleteAll(0.1)
-            .typeString(nextData.text)
-            .start()
-            .callFunction(()=>{
-                if(nextData.thereIsQuestion){}
-                else{enableArrow()}
-            });
+        changeText(nextData.text, nextData.thereIsQuestion)
+
     }
 }
 
+function changeText(text, thereIsQuestion){
+    typewriter
+    .callFunction(()=>{
+        disableArrow()
+    })
+    .deleteAll(0.1)
+    .typeString(text)
+    .start()
+    .callFunction(()=>{
+        if(thereIsQuestion){}
+        else{enableArrow()}
+    });
+}
+
 function showQuestion(){
-    var questionNow = questionData.shift();
+    questionNow = questionData.shift();
     box_question_text.innerHTML = questionNow.questionText;
     box_question.style.display = "block"
+}
+
+function verifyAnswer(answer){
+
+    // Se o input estiver vazio
+    if(answer == ""){
+        var input_answer = document.getElementById("input-answer");
+        blink(input_answer);
+    }else{
+        // Se for a resposta correta
+        if(answer == questionNow.correctAnswer){
+            playAudio(audioFiles.piter_comemoracao, 0.3);
+            totalCorrectAnswers++;
+            changeText(questionNow.correctFeedback, false)
+        }else{
+            playAudio(audioFiles.piter_erro, 0.3)
+            changeText(questionNow.wrongFeedback, false)
+        }
+        hideBoxQuestion();
+    }
 }
 
 function callBlackScreen (fadeInDuration, fadeOutDuration, blackScreenDuration, thereIsQuestion){
@@ -126,13 +157,30 @@ function callBlackScreen (fadeInDuration, fadeOutDuration, blackScreenDuration, 
     blackScreen.classList.remove("fade-out");
 
 }
+function blink(input_answer) {
+    var interval = setInterval(function() {
+        if (input_answer.style.backgroundColor === "yellow") {
+            input_answer.style.backgroundColor = "transparent";
+        } else {
+            input_answer.style.backgroundColor = "yellow";
+        }
+    }, 500); // Intervalo de 500 milissegundos (0.5 segundos)
+
+    // Após 3 segundos, para de piscar
+    setTimeout(function() {
+        clearInterval(interval);
+        input_answer.style.backgroundColor = "transparent"; // Retorna à cor padrão
+    }, 3000); // Duração total do piscar (3 segundos)
+}
 
 btn_arrow.addEventListener("click", ()=>{
+    playAudio(audioFiles.button_next, 0.4)
     nextStep();
 });
+
 btn_done.addEventListener("click", ()=>{
+    var input_answer = document.getElementById("input-answer");
     var answer = input_answer.value;
-    console.log(answer);
-    nextStep();
+    verifyAnswer(answer)
 });
 
