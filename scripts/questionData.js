327b6c83064c63cc6e51c18179b39a9a2ce45ccb
gabrielbiscaia <ra118928@uem.br>
2024-02-22 13:57:22 -0300

    criação do arquivo questionData.js

---
+++ scripts/questionData.js
@@ -0,0 +1,13 @@
+var questionData = [
+    {
+        questionText:
+        "<pre>"+
+        "<input id='input-answer' type='text' maxlength='5' spellcheck='false'>(chuveiro == 'desligado'){<br>"+
+        "   entrar_no_banho('piter')<br>"+
+        "   ligar_chuveiro();<br>"+
+        "}"+
+    "</pre>"
+    }
+]
+
+export { questionData };
